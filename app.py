56d7761a3df11d24db12493eab4b4a34b77a9cc2
Coding-Tom-1405 <156367458+Coding-Tom-1405@users.noreply.github.com>
2024-03-02 18:41:44 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask, render_template, request
-from flask import redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/") # decorators
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+@app.route("/")
+def homepage():
+    return """<h1> Welcome to the <b> Chimp Test </b> Game!</h1>
+   <a href="/select">Start Game!</a>"""
+
+@app.route("/select")
+def new():
+    return render_template('select2.html')
+
+@app.route('/reselect', methods=['GET', 'POST'])
+def reselect():
+    if request.method == 'POST':
+        selected = int(request.form["level"])
+        return render_template("reselect3.html", selected=selected)
+    else:
+        # Handle GET request if needed
+        pass
+
+
+# @app.route('/confirm', methods=['POST'])
+# def confirm():
+#     selected = int(request.form["level"])
+#     challenged = int(request.form["challenge"])
+#     return render_template("confirm4.html", selected=selected, challenged=challenged)
+
+if __name__ == "__main__":
+    app.run(debug=True)
