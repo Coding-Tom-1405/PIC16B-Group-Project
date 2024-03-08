1dd85e2ffc664acea667349d472a280eb6dd5a09
askpl00 <123619540+askpl00@users.noreply.github.com>
2024-03-07 22:59:57 -0800

    Update pygame.py

--- pygame.py
+++ pygame.py
@@ -4,36 +4,54 @@ from glob import glob
 import os
 
 # all the possible positions for the numbers
-pos = [(x, y) for x in range(1, 8) for y in range(1, 8)] 
-# print(pos)
-# print(pos.pop(pos.index(random.choice(pos))))
-# print(pos)
+coord = [(x, y) for x in range(1, 10) for y in range(1, 10)] 
+original_coord = coord[:]
 
 def game_init():
     global screen, font
 
     pygame.init()
-    size = w, h = 800, 800
-    screen = pygame.display.set_mode((size))
-    pygame.display.set_caption("Chimp Test")
+    screen = pygame.display.set_mode((800, 800))
+    pygame.display.set_caption("Chimp Memory Test")
     font = pygame.font.SysFont("Times New Roman", 35)
-    color = (255, 0, 0)
  
     # Changing surface color
-    screen.fill(color)
-    pygame.display.flip()
+    screen.fill((0,0,255))
+#     pygame.display.flip()
 
 
 class Square(pygame.sprite.Sprite):
-    def __init__(self, number):
+     
+    def __init__(self, number, make_image=True):
         super(Square, self).__init__()
         self.number = number
+        if make_image:
+            self.make_image()
+        
+        
+    def update(self):
+        
         self.make_image()
+        screen.blit(self.image, (self.x, self.y))
 
+        
+    def random_coord(self):
+        
+        global coord  # Access the global pos list
+
+        if not coord:
+            # Regenerate pos list if it's empty
+            coord.extend(original_coord)
+        coordinates = random.choice(coord)
+        x, y = coordinates
+        coord.remove(coordinates)  # Remove the chosen position from the list
+        x = x * 80
+        y = y * 80
+        return x, y
+    
     def make_image(self):
         global font
-
-        self.x, self.y = self.random_pos()
+        self.x, self.y = self.random_coord()
         self.image = pygame.Surface((80, 80))
         self.image.fill((255, 255, 255))
         self.rect = self.image.get_rect()
@@ -43,73 +61,65 @@ class Square(pygame.sprite.Sprite):
         text_rect = self.text.get_rect(center=(80 // 2, 80 // 2))
         self.image.blit(self.text, text_rect)
 
-    def update(self):
-        self.make_image()
-        screen.blit(self.image, (self.x, self.y))
-
-    def random_pos(self):
-        position = random.choice(pos)
-        x, y = position
-        pos.pop(pos.index(position))
-        # x = random.randrange(0, 16)
-        # y = random.randrange(0, 16)
-        x = x * 80
-        y = y * 80
-
-        return x, y
-
-
-
-g = pygame.sprite.Group()
-num_order = []
+square_group = pygame.sprite.Group()
+num_list = []
 level = 1
+incorrect_guesses = 0
 # This covers the numbers...
 bgd = pygame.Surface((80, 80))
-# bgd.fill((255, 0, 0))
-
+# sprite_coordinate = []
 
 def hide_cards():
-    for sprite in g:
+    for sprite in square_group:
         bgd.fill((255, 255, 255))
         sprite.image.blit(bgd, (0, 0))
+def reset_coord():
+    global coord, original_coord
+    coord = original_coord[:]  # Reset pos to its original state        
 
 
-def mouse_collision(sprite):
-    global num_order, level, counter_on, max_count, cards_visible
 
+def memory(sprite):
+    global num_list, level, timer_on, max_timer, cards_visible, incorrect_guesses, loop
     # Check the collision only when conter is off
-    x, y = pygame.mouse.get_pos()
+    x, y = pygame.mouse.get_pos() 
     if sprite.rect.collidepoint(x, y):
-        # bgd.fill((0, 255, 0))
-        # sprite.image.blit(bgd, (0, 0))
-        num_order.append(sprite.number)
+#         play()#"click")
+        num_list.append(sprite.number)
         sprite.rect = pygame.Rect(-80, -80, 80, 80)
 
         # Check if you are wrong as you type
-        if sprite.number != str(len(num_order)):
-            num_order = []
-            counter_on = 1
-            # pygame.mouse.set_visible(False)
-            g.update()
-            bgd.fill((255, 0, 0))
-            screen.fill((255, 0, 0))  # Fill the screen with red color
-            pygame.display.flip()  # Update the display to show the green screen
-            pygame.time.wait(500)  # Wait for 200 milliseconds
-            screen.fill((0, 0, 0))  # Fill the screen with black color again
-            pygame.display.flip()  # Update the display to show the black screen
-
-    if len(num_order) == len(g):
-        print("fine")
-        print(num_order)
-        # =========================== YOU GUESSED ========== 
-        win = num_order == [str(s.number) for s in g]
-        if win:
-            level += 1
+        if sprite.number != str(len(num_list)):
             print(str(level))
-            num_order = []
-            g.add(Square(len(g) + 1))
-            counter_on = 1
-            max_count = max_count + 10
+            print("incorrect")
+            reset_coord()
+            incorrect_guesses += 1
+            if incorrect_guesses >= 3:
+                loop = 0  # Terminate the game loop
+            num_list = []
+            timer_on = 1
+            square_group.update()
+            screen.fill((255, 0, 0))  # Fill the screen with red color
+            pygame.display.flip()  # Update the display
+            pygame.time.wait(500)  # Wait for 500 milliseconds
+  
+
+
+
+
+    if len(num_list) == len(square_group):
+        # =========================== YOU GUESSED ========== 
+        win = num_list == [str(squares.number) for squares in square_group]
+        if win:   
+            print(str(level))
+            level += 1
+            print("correct")
+            reset_coord()
+            
+            num_list = []
+            square_group.add(Square(len(square_group) + 1, make_image = False))
+            timer_on = 1
+            max_timer += 10
             cards_visible = 1
             bgd.fill((255,0,0))
             screen.fill((0, 255, 0))  # Fill the screen with green color
@@ -117,60 +127,111 @@ def mouse_collision(sprite):
             pygame.time.wait(500)  # Wait for 200 milliseconds
             screen.fill((0, 0, 0))  # Fill the screen with black color again
             pygame.display.flip()  # Update the display to show the black screen
+            
+            
         else:
-            num_order = []
-            counter_on = 1
+            print(str(level))
+            print("incorrect")
+            reset_coord()
+            incorrect_guesses += 1
+            if incorrect_guesses >= 3:
+                loop = 0  # Terminate the game loop
+            num_list = []
+            timer_on = 1
             cards_visible = 1
-            # pygame.mouse.set_visible(False)
-            bgd.fill((255,0,0))
-        g.update()
-        screen.fill((0,0,0))
+            screen.fill((255, 0, 0))  # Fill the screen with red color
+           
+        square_group.update()
+        print("-"*25)
+        screen.fill((0, 0, 0))  # Fill the screen with black color
         
+
+
+######################
+#     sound          #
+######################
+def init():
+    "Initializing pygame and mixer"
+#     pygame.mixer.pre_init(44100, -16, 1, 512)
+    pygame.init()
+#     pygame.mixer.quit()
+#     pygame.mixer.init(22050, -16, 2, 512)
+#     pygame.mixer.set_num_channels(32)
+    # Load all sounds
+#     lsounds = glob("sounds\\*.mp3")
+#     print(lsounds)
+#     # Dictionary with all sounds, keys are the name of wav
+#     sounds = {}
+#     for sound in lsounds:
+#         sounds[sound.split("\\")[1][:-4]] = pygame.mixer.Sound(f"{sound}")
+#     return sounds
+# =========================== ([ sounds ]) ============
+
+# sounds = init()
+
+# base = pygame.mixer.music
+# def soundtrack(filename, stop=0):
+#     "This load a base from sounds directory"
+#     base.load(filename)
+#     if stop == 1:
+#         base.stop()
+#     else:
+#         base.play(-1)
+
+#def play():#sound):
+#     pygame.mixer.Sound.play(sounds[sound])
+    return None
 def squares_init():
     for i in range(1, 4):
-        g.add(Square(i))
+        square_group.add(Square(i))
 
 counter = 0
-counter_on = 1
-max_count = 100
+timer_on = 1
+max_timer = 200
 cards_visible = 1
 
-def get_maxscore() -> int:
-    filename = "maxscore.txt"
+def get_maxlevel() -> int:
+    filename = "maxlevel.txt"
     if filename in os.listdir():
         with open(filename, "r") as file:
-            val = file.read()
-            if val == "":
-                maxscore = 0
+            leader = file.read()
+            if leader == "":
+                maxlevel = 0
             else:
-                maxscore = int(val)
+                maxlevel = int(leader)
     else:
-        maxscore = 0
-    return maxscore
+        maxlevel = 0
+    return maxlevel
 
-def set_score(maxscore) -> None:
+def set_maxlevel(maxlevel) -> None:
+    with open("maxlevel.txt", "w") as file:
+        file.write(str(maxlevel))
 
-    with open("maxscore.txt", "w") as file:
-        file.write(str(maxscore))
-maxscore = get_maxscore()
+def update_maxlevel(level):
+    maxlevel = get_maxlevel()
+    if level > maxlevel:
+        set_maxlevel(level)
 
 def main():
-    global counter_on, counter, max_count, cards_visible
+    global timer_on, counter, max_timer, cards_visible, loop
 
     game_init()
     squares_init()
     clock = pygame.time.Clock()
+
     loop = 1
     # pygame.mouse.set_visible(False)
 #     soundtrack("sounds/soave.ogg")
     while loop:
-        screen.fill((0, 0, 0))
+        screen.fill((0,0,255))
         text = font.render("Level: " + str(level), 1, (255, 255, 255))
         screen.blit(text, (0, 0))
-        if counter_on:
-            text = font.render(" Time: " + str(max_count - counter), 1, (255, 255, 255))
+        if timer_on:
+            text = font.render(" Time: " + str(max_timer - counter), 1, (255, 255, 255))
             screen.blit(text, (200, 0))
             counter += 1
+#             if counter % 4 == 0:
+#                 play()#"click")
         for event in pygame.event.get():
             # ========================================= QUIT
             if event.type == pygame.QUIT:
@@ -178,30 +239,30 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_s:
                     loop = 0
-                    screen.fill((0,0,0))
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # Click mouse and stop the timer and hide the cards
-                for s in g:
-                    if s.rect.collidepoint(event.pos):
+                for squares in square_group:
+                    if squares.rect.collidepoint(event.pos):
                         hide_cards()
                         cards_visible = 0
-                        counter_on = 0
+                        timer_on = 0
                         counter = 0
-                        mouse_collision(s)    
+                        memory(squares)    
 
 
-        g.draw(screen)
+        square_group.draw(screen)
         # Hides the number...
-        if counter == max_count:
-            hide_cards()
+        if counter == max_timer:
+#             hide_cards()
             counter = 0
-            counter_on = 0
+            timer_on = 0
 
                 # pygame.mouse.set_visible(True)
         pygame.display.update()
         clock.tick(20)
-
+                
     pygame.quit()
 
-
+    
 main()
+update_maxlevel(level)
